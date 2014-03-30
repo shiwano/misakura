175080e12f1a6fec37b5f8d6b2856cbefbecec54
Shogo Iwano <shiwano@gmail.com>
2014-03-30 10:11:27 +0900

    パイプを使ったテストがひゃぁんと書けていぃにゃかったのぉおおれ修正しましたのぉおお
    Before translating into Misakura language:
    	パイプを使ったテストがちゃんと書けていなかったので修正しました

--- misakura-test.sh
+++ misakura-test.sh
@@ -6,22 +6,22 @@ misakura="./misakura"
 
 it_translate_string_data() {
   misakurized=`$misakura 本日はお招きいただきありがとうございます`
-  test "${misakurized}" = '本日はおﾞぉおォおん招きいぃたらきぁあああ あぉりがとうごじゃいぃましゅぅぅぅ'
+  test "$misakurized" = '本日はおﾞぉおォおん招きいぃたらきぁあああ あぉりがとうごじゃいぃましゅぅぅぅ'
 }
 
 it_translate_text_data() {
   misakurized=`$misakura "「ごきげんよう」\n「ごきげんよう」\nさわやかな朝の挨拶が、澄みきった青空にこだまする。"`
   expected=`printf "「ごきげんよぉおお！んおっ！んおおーーっ！ 」\n「ごきげんみゃぁあ゛あ゛ぁ゛ぁぁあ！！」\nしゃわやかにゃ朝のぉおお挨拶が、澄みきった青空にこらましゅぅぅぅるのぉおお。"`
-  test "${misakurized}" = "${expected}"
+  test "$misakurized" = "$expected"
 }
 
 it_translate_pipe_string_data() {
-  misakurized=`$misakura たまには運動するのも気持ちいいですね`
-  test "${misakurized}" = 'たまには運動しゅるのぉおおのぉおおもぎも゛ぢいぃ゛いぃ゛ぃれしゅぅぅぅね'
+  misakurized=`printf たまには運動するのも気持ちいいですね | $misakura`
+  test "$misakurized" = 'たまには運動しゅるのぉおおのぉおおもぎも゛ぢいぃ゛いぃ゛ぃれしゅぅぅぅね'
 }
 
 it_translate_pipe_text_data() {
-  misakurized=`$misakura "君からメールが届いた\n「元気？」って\nたった今元気になった"`
+  misakurized=`printf "君からメールが届いた\n「元気？」って\nたった今元気になった" | $misakura`
   expected=`printf "君からメールが届いぃたのぉおお\n「元気？」って\nたった今元気ににゃったのぉおお"`
-  test "${misakurized}" = "${expected}"
+  test "$misakurized" = "$expected"
 }
